d4552df7e5f97cf9bcb0a79c43bfe8b20324b078
mattm1346 <MATTM1346@gmail.com>
2021-08-14 15:54:55 +0000

    Add structure for game

--- run.py
+++ run.py
@@ -1,1 +1,30 @@
-# python code goes here
+print(
+    '''  ____       _______ _______ _      ______  _____ _    _ _____ _____   _____ 
+|  _ \   /\|__   __|__   __| |    |  ____|/ ____| |  | |_   _|  __ \ / ____|
+| |_) | /  \  | |     | |  | |    | |__  | (___ | |__| | | | | |__) | (___  
+|  _ < / /\ \ | |     | |  | |    |  __|  \___ \|  __  | | | |  ___/ \___ \ 
+| |_) / ____ \| |     | |  | |____| |____ ____) | |  | |_| |_| |     ____) |
+|____/_/    \_\_|     |_|  |______|______|_____/|_|  |_|_____|_|    |_____/ 
+''')
+
+# Define lists and variables for actions
+
+# Set amount of ships
+
+# Computer creates board
+
+# User creates board
+
+# Create loop for game
+
+# Player shoots
+
+# Check player shot
+
+# Repeat loop until ships empty
+
+# Computer shoots
+
+# Check computer shot
+
+# Repeat loop until ships empty
